2244765a6d9f6ec6e086b9423a156d99fed29865
saumyapprakash30 <saumyaprakash30@gmail.com>
2021-05-04 00:49:48 +0530

    add bootstrap; start button issue

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -74,7 +74,7 @@ function joinRoom(){
             document.getElementById('waiting').style.display = 'block';
             document.getElementById('chat').style.display = 'block';
             document.getElementById('userList').style.display = 'block';
-            document.getElementById('roomDetail').innerHTML= '<h3>Room Id - '+saveRoomId+'</h3>'+'<span >Invite link : </span><br><span  id="urlToShare" style="background-color: #1db0ff;margin: 0 auto" >'+urlToShare+'</span><br><button onclick="copyLink()">copyLink</button>';
+            document.getElementById('roomDetail').innerHTML= '<h3>Room Id - '+saveRoomId+'</h3>'+'<span >Invite link : </span><br><span  id="urlToShare" style="background-color: #1db0ff;margin: 0 auto" >'+urlToShare+'</span><br><button onclick="copyLink()" type="button" class="btn btn-info">copyLink</button>';
         }
     });
 }
@@ -364,3 +364,34 @@ socket.on('disconnect',()=>{
     alert('Server disconnected! Please reconnect.');
     window.location = '/';
 })
+
+////////////////////design//////////////////
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
